519e644904246a562b426964e883ef00d66b3ddb
Ghislain Lacombe <60610113+GhislainLacombe@users.noreply.github.com>
2023-04-25 15:50:43 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,471 @@
+var image01 = document.getElementById("image01");
+var image02 = document.getElementById("image02");
+var image03 = document.getElementById("image03");
+var image04 = document.getElementById("image04");
+var image05 = document.getElementById("image05");
+var image06 = document.getElementById("image06");
+var image07 = document.getElementById("image07");
+var image08 = document.getElementById("image08");
+var image09 = document.getElementById("image09");
+var image10 = document.getElementById("image10");
+var Rei = document.getElementById("Rei");
+var fanatic = document.getElementById("fanatic");
+myText01 = setTimeout(myFunction01, 5000);
+myText02 = setTimeout(myFunction02, 5150);
+myText03 = setTimeout(myFunction03, 5300);
+myText01 = setTimeout(myFunction01, 5450);
+myText02 = setTimeout(myFunction02, 5600);
+myText03 = setTimeout(myFunction03, 5750);
+myText04 = setTimeout(myFunction04, 5900);
+myText05 = setTimeout(myFunction05, 6050);
+myText06 = setTimeout(myFunction06, 6200);
+myText04 = setTimeout(myFunction04, 6350);
+myText05 = setTimeout(myFunction05, 6500);
+myText06 = setTimeout(myFunction06, 6650);
+myText07 = setTimeout(myFunction07, 6800);
+myText08 = setTimeout(myFunction08, 6950);
+myText09 = setTimeout(myFunction09, 7100);
+myText10 = setTimeout(myFunction10, 7250);
+myText11 = setTimeout(myFunction11, 7400);
+myText12 = setTimeout(myFunction12, 7550);
+myText13 = setTimeout(myFunction13, 7700);
+myText14 = setTimeout(myFunction14, 8000);
+myText15 = setTimeout(myFunction15, 8200);
+myText14 = setTimeout(myFunction14, 8250);
+myText15 = setTimeout(myFunction15, 8400);
+myText14 = setTimeout(myFunction14, 8450);
+myText15 = setTimeout(myFunction15, 8600);
+myText16 = setTimeout(myFunction16, 9000);
+myText17 = setTimeout(myFunction17, 9150);
+myText18 = setTimeout(myFunction18, 9300);
+myText16 = setTimeout(myFunction16, 9450);
+myText17 = setTimeout(myFunction17, 9600);
+myText18 = setTimeout(myFunction18, 9750);
+myText19 = setTimeout(myFunction19, 9900);
+myText20 = setTimeout(myFunction20, 10050);
+myText21 = setTimeout(myFunction21, 10200);
+myText22 = setTimeout(myFunction22, 10350);
+myText23 = setTimeout(myFunction23, 11500);
+myText24 = setTimeout(myFunction24, 14500);
+myText25 = setTimeout(myFunction25, 14600);
+myText26 = setTimeout(myFunction26, 14700);
+myText27 = setTimeout(myFunction27, 14800);
+myText28 = setTimeout(myFunction28, 14900);
+myText29 = setTimeout(myFunction29, 15000);
+myText30 = setTimeout(myFunction30, 15100);
+myText31 = setTimeout(myFunction31, 15200);
+myText32 = setTimeout(myFunction32, 15300);
+myText33 = setTimeout(myFunction33, 15400);
+myText34 = setTimeout(myFunction34, 15500);
+myText35 = setTimeout(myFunction35, 15600);
+myText36 = setTimeout(myFunction36, 15900);
+myText35 = setTimeout(myFunction35, 16200);
+myText36 = setTimeout(myFunction36, 16500);
+myText35 = setTimeout(myFunction35, 16800);
+myText36 = setTimeout(myFunction36, 17100);
+myText35 = setTimeout(myFunction35, 17400);
+myText36 = setTimeout(myFunction36, 17700);
+myText35 = setTimeout(myFunction35, 18000);
+myText36 = setTimeout(myFunction36, 18300);
+myText35 = setTimeout(myFunction35, 18600);
+myText36 = setTimeout(myFunction36, 18900);
+myText37 = setTimeout(myFunction37, 19200);
+myText38 = setTimeout(myFunction38, 19350);
+myText39 = setTimeout(myFunction39, 19500);
+myText40 = setTimeout(myFunction40, 19650);
+myText41 = setTimeout(myFunction41, 19800);
+myText42 = setTimeout(myFunction42, 19950);
+myText43 = setTimeout(myFunction43, 20100);
+myText44 = setTimeout(myFunction44, 21000);
+myText45 = setTimeout(myFunction45, 21100);
+myText46 = setTimeout(myFunction46, 21600);
+myImage01 = setTimeout(myImage01, 23000);
+myImage02 = setTimeout(myImage02, 28000);
+myImage03 = setTimeout(myImage03, 30500);
+myImage04 = setTimeout(myImage04, 35500);
+myImage05 = setTimeout(myImage05, 38000);
+myImage06 = setTimeout(myImage06, 43000);
+myImage07 = setTimeout(myImage07, 45500);
+myImage08 = setTimeout(myImage08, 50500);
+myImage09 = setTimeout(myImage09, 53000);
+myImage10 = setTimeout(myImage10, 58000);
+myImage11 = setTimeout(myImage11, 60500);
+myImage12 = setTimeout(myImage12, 65500);
+myImage13 = setTimeout(myImage13, 68000);
+myImage14 = setTimeout(myImage14, 73000);
+myImage15 = setTimeout(myImage15, 75500);
+myImage16 = setTimeout(myImage16, 80500);
+myImage17 = setTimeout(myImage17, 83000);
+myImage18 = setTimeout(myImage18, 88000);
+myImage19 = setTimeout(myImage19, 90500);
+myImage20 = setTimeout(myImage20, 95500);
+myImage21 = setTimeout(myImage21, 98000);
+myImage22 = setTimeout(myImage22, 123000);
+
+
+
+function myFunction01() {
+  document.getElementById("text01").innerHTML = "You don't get anything, #nma.";
+  MyBug01 = setInterval(bugPosition01, 700);
+  MyBug02 = setInterval(bugPosition02, 1000);
+  MyBug03 = setInterval(bugPosition03, 1500);
+
+}
+
+function myFunction02() {
+  document.getElementById("text01").innerHTML = "You don't get anything, %nma.";
+}
+
+function myFunction03() {
+  document.getElementById("text01").innerHTML = "You don't get anything, $nma.";
+  document.getElementById("bug01").style.opacity = "15%";
+}
+
+function myFunction04() {
+  document.getElementById("text01").innerHTML = "You don't get anything, M#ma.";
+  document.getElementById("bug01").style.top = "15%";
+  document.getElementById("bug01").style.left = "5%";
+}
+
+function myFunction05() {
+  document.getElementById("text01").innerHTML = "You don't get anything, M%ma.";
+  document.getElementById("bug01").style.top = "35%";
+  document.getElementById("bug01").style.left = "55%";
+}
+
+function myFunction06() {
+  document.getElementById("text01").innerHTML = "You don't get anything, M$ma.";
+}
+
+function myFunction07() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Mo#a.";
+}
+
+function myFunction08() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Mo%a.";
+  document.getElementById("bug01").style.top = "25%";
+  document.getElementById("bug01").style.left = "25%";
+  document.getElementById("bug01").style.opacity = "35%";
+
+}
+
+function myFunction09() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Mo$a.";
+}
+
+function myFunction10() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Moo#.";
+}
+
+function myFunction11() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Moo%.";
+  document.getElementById("bug01").style.top = "15%";
+  document.getElementById("bug01").style.left = "5%";
+  document.getElementById("bug01").style.opacity = "15%";
+
+}
+
+function myFunction12() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Moo$.";
+}
+
+function myFunction13() {
+  document.getElementById("text01").innerHTML = "You don't get anything, Moon.";
+}
+
+function myFunction14() {
+  document.getElementById("text01").innerHTML = "You don't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction15() {
+  document.getElementById("text01").innerHTML = "You don't get anything, <a class='text03'>Moon</a>.";
+  document.getElementById("bug01").style.top = "35%";
+  document.getElementById("bug01").style.left = "55%";
+}
+
+function myFunction16() {
+  document.getElementById("text01").innerHTML = "You #on't get anything, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "15%";
+  document.getElementById("bug01").style.left = "5%";
+}
+
+function myFunction17() {
+  document.getElementById("text01").innerHTML = "You %on't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction18() {
+  document.getElementById("text01").innerHTML = "You $on't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction19() {
+  document.getElementById("text01").innerHTML = "You c#n't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction20() {
+  document.getElementById("text01").innerHTML = "You c%n't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction21() {
+  document.getElementById("text01").innerHTML = "You c$n't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction22() {
+  document.getElementById("text01").innerHTML = "You can't get anything, <a class='text02'>Moon</a>.";
+}
+
+function myFunction23() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n̶y̶t̶h̶i̶n̶g, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "25%";
+  document.getElementById("bug01").style.left = "25%";
+}
+
+function myFunction24() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n̶y̶t̶h̶i̶n|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction25() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n̶y̶t̶h̶i|, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "15%";
+  document.getElementById("bug01").style.left = "5%";
+}
+
+function myFunction26() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n̶y̶t̶h|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction27() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n̶y̶t|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction28() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n̶y|, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "25%";
+  document.getElementById("bug01").style.left = "25%";
+}
+
+function myFunction29() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a̶n|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction30() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ a|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction31() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t̶ |, <a class='text02'>Moon</a>.";
+}
+
+function myFunction32() {
+  document.getElementById("text01").innerHTML = "You can't g̶e̶t|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction33() {
+  document.getElementById("text01").innerHTML = "You can't g̶e|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction34() {
+  document.getElementById("text01").innerHTML = "You can't g|, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "15%";
+  document.getElementById("bug01").style.left = "5%";
+}
+
+function myFunction35() {
+  document.getElementById("text01").innerHTML = "You can't |, <a class='text02'>Moon</a>.";
+}
+
+function myFunction36() {
+  document.getElementById("text01").innerHTML = "You can't  , <a class='text02'>Moon</a>.";
+}
+
+function myFunction37() {
+  document.getElementById("text01").innerHTML = "You can't e|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction38() {
+  document.getElementById("text01").innerHTML = "You can't es|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction39() {
+  document.getElementById("text01").innerHTML = "You can't esc|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction40() {
+  document.getElementById("text01").innerHTML = "You can't esca|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction41() {
+  document.getElementById("text01").innerHTML = "You can't escap|, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "25%";
+  document.getElementById("bug01").style.left = "25%";
+}
+
+function myFunction42() {
+  document.getElementById("text01").innerHTML = "You can't escape|, <a class='text02'>Moon</a>.";
+}
+
+function myFunction43() {
+  document.getElementById("text01").innerHTML = "You can't escape, <a class='text02'>Moon</a>.";
+  document.getElementById("bug01").style.top = "35%";
+  document.getElementById("bug01").style.left = "55%";
+}
+
+function myFunction44() {
+  document.getElementById("text01").innerHTML = "You can't <a class='select'>escape</a>, <a class='text02'>Moon</a>.";
+}
+
+function myFunction45() {
+  document.getElementById("text01").innerHTML = "You can't <a class='selectbig'>escape</a>, <a class='text02'>Moon</a>.";
+}
+
+function myFunction46() {
+  document.getElementById("text01").innerHTML = "You can't <a class='big'>escape</a>, <a class='text02'>Moon</a>.";
+}
+
+function bugPosition01() {
+  document.getElementById("bug01").style.top = "25%";
+  document.getElementById("bug01").style.left = "25%";
+  document.getElementById("bug01").style.opacity = "35%";
+  document.getElementById("bug02").style.top = "45%";
+  document.getElementById("bug02").style.left = "65%";
+  document.getElementById("bug02").style.opacity = "15%";
+  document.getElementById("bug03").style.top = "55%";
+  document.getElementById("bug03").style.left = "5%";
+  document.getElementById("bug03").style.opacity = "20%";
+  document.getElementById("bug04").style.top = "90%";
+  document.getElementById("bug04").style.left = "65%";
+  document.getElementById("bug04").style.opacity = "20%";
+}
+
+function bugPosition02() {
+  document.getElementById("bug01").style.top = "70%";
+  document.getElementById("bug01").style.left = "60%";
+  document.getElementById("bug01").style.opacity = "15%";
+  document.getElementById("bug02").style.top = "85%";
+  document.getElementById("bug02").style.left = "35%";
+  document.getElementById("bug02").style.opacity = "25%";
+  document.getElementById("bug03").style.top = "5%";
+  document.getElementById("bug03").style.left = "75%";
+  document.getElementById("bug03").style.opacity = "10%";
+  document.getElementById("bug04").style.top = "10%";
+  document.getElementById("bug04").style.left = "5%";
+  document.getElementById("bug04").style.opacity = "15%";
+}
+
+function bugPosition03() {
+  document.getElementById("bug01").style.top = "10%";
+  document.getElementById("bug01").style.left = "40%";
+  document.getElementById("bug01").style.opacity = "5%";
+  document.getElementById("bug02").style.top = "55%";
+  document.getElementById("bug02").style.left = "55%";
+  document.getElementById("bug02").style.opacity = "25%";
+  document.getElementById("bug03").style.top = "35%";
+  document.getElementById("bug03").style.left = "35%";
+  document.getElementById("bug03").style.opacity = "15%";
+  document.getElementById("bug04").style.top = "60%";
+  document.getElementById("bug04").style.left = "75%";
+  document.getElementById("bug04").style.opacity = "5%";
+}
+
+function myImage01() {
+  image01.classList.add("fadeIn");
+}
+
+function myImage02() {
+  image01.classList.remove("fadeIn");
+  image01.classList.add("fadeOut");
+}
+
+function myImage03() {
+  image02.classList.add("fadeIn");
+}
+
+function myImage04() {
+  image02.classList.remove("fadeIn");
+  image02.classList.add("fadeOut");
+}
+
+function myImage05() {
+  image03.classList.add("fadeIn");
+}
+
+function myImage06() {
+  image03.classList.remove("fadeIn");
+  image03.classList.add("fadeOut");
+}
+
+function myImage07() {
+  image04.classList.add("fadeIn");
+}
+
+function myImage08() {
+  image04.classList.remove("fadeIn");
+  image04.classList.add("fadeOut");
+}
+
+function myImage09() {
+  image05.classList.add("fadeIn");
+}
+
+function myImage10() {
+  image05.classList.remove("fadeIn");
+  image05.classList.add("fadeOut");
+}
+
+function myImage11() {
+  image06.classList.add("fadeIn");
+}
+
+function myImage12() {
+  image06.classList.remove("fadeIn");
+  image06.classList.add("fadeOut");
+}
+
+function myImage13() {
+  image07.classList.add("fadeIn");
+}
+
+function myImage14() {
+  image07.classList.remove("fadeIn");
+  image07.classList.add("fadeOut");
+}
+
+function myImage15() {
+  image08.classList.add("fadeIn");
+}
+
+function myImage16() {
+  image08.classList.remove("fadeIn");
+  image08.classList.add("fadeOut");
+}
+
+function myImage17() {
+  image09.classList.add("fadeIn");
+}
+
+function myImage18() {
+  image09.classList.remove("fadeIn");
+  image09.classList.add("fadeOut");
+}
+
+function myImage19() {
+  image10.classList.add("fadeIn");
+}
+
+function myImage20() {
+  image10.classList.remove("fadeIn");
+  image10.classList.add("fadeOut");
+}
+
+function myImage21() {
+  Rei.classList.add("fadeIn2");
+  fanatic.classList.add("fadeIn2");
+}
+
+function myImage22() {
+  Rei.classList.remove("fadeIn2");
+  fanatic.classList.remove("fadeIn2");
+  document.getElementById("Rei").style.opacity = "100%";
+  document.getElementById("fanatic").style.opacity = "100%";
+
+}
+
